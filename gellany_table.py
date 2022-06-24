9dbdb3f05c3b949a908ac10bc6c691e261c74ba8
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-24 18:17:13 +0000

    Add files via upload

--- gellany_table.py
+++ gellany_table.py
@@ -22,10 +22,11 @@ class gellany_table():
               def driver(self):
 
                          options = Options()
-                         options.add_argument("no-sandbox")
-                         options.add_argument("headless")
-                         options.add_argument("start-maximized")
-                         options.add_argument("window-size=1900,1080"); 
+                         options.add_argument("no-sandbox");
+                         options.add_argument("headless");
+                         options.add_argument("start-maximized");
+                         options.add_argument("window-size=1900,1080");
+                         options.add_argument("127.0.0.1:4444");
                          global driver
                          driver = webdriver.Chrome(chrome_options=options, executable_path="/usr/bin/chromedriver")
                          driver.implicitly_wait(0.5)
